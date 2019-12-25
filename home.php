b62cee00c7343272828b8f0f59295c0d176be0f8
YannickBR <yannickbruintjes@hotmail.com>
2019-12-25 20:52:39 +0100

    begin

---
+++ home.php
@@ -0,0 +1,15 @@
+<h1>Welcome to the Hypixel Skyblock Minion Money Calculator!</h1>
+<p>Here you can calculate the amount of money you will make with a minion per hour, day, week or even a year!</p>
+<link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<form action="calculate.php">
+
+	<label>Default Action Time (in seconds)</label><br><input name="default_action_time" value="">
+	<br><br>
+	<label>Booster (in percentages). If none, please enter "0". For example if the booster makes the minion 20% faster enter "20"</label><br><input name="boost" value="">
+	<br><br>
+	<label>Sell Price. The sell price of the item the minion generates.</label><br><input name="sell_price" value="">
+	<br><br>
+	<input type="submit" value="Submit" class="submit">
+</form>
